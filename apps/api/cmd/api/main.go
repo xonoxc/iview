12d85bf676c0243cae8c5fe48c1d9badc0a372a7
arpit <arpit2003y@gmail.com>
2026-08-02 23:25:37 +0530

    chore(containers):app_containers_ready

--- apps/api/cmd/api/main.go
+++ apps/api/cmd/api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+
+	"github.com/xonoxc/iview/apps/api/internal/server"
+)
 
 func main() {
-	fmt.Println("api will be here")
+	sigCtx, cancel := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT,
+		syscall.SIGKILL,
+	)
+	defer cancel()
+
+	port := ":8080"
+
+	s := server.New(port)
+
+	log.Println("server is running on:", port)
+
+	if err := s.Start(sigCtx); err != nil {
+		log.Printf("server error: %v", err)
+	}
 }
---
+++ apps/api/internal/server/server.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	db   *sql.DB
+	port string
+}
+
+func New(port string) *Server {
+	return &Server{
+		port: port,
+	}
+}
+
+func (s *Server) Start(ctx context.Context) error {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handleRoot)
+
+	server := &http.Server{
+		Addr:    s.port,
+		Handler: mux,
+	}
+
+	go func() {
+		<-ctx.Done()
+
+		shutdownCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		server.Shutdown(shutdownCtx)
+	}()
+
+	err := server.ListenAndServe()
+
+	if errors.Is(err, http.ErrServerClosed) {
+		return nil
+	}
+
+	return err
+}
+
+func handleRoot(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("hello\n"))
+}
