5876c91379045748a23e48ce63e0dfa33de63703
arpit <arpit2003y@gmail.com>
2026-08-25 11:15:25 +0530

    chore(minor):formatting_changes

--- apps/api/internal/server/server.go
+++ apps/api/internal/server/server.go
@@ -30,7 +30,10 @@ func (s *Server) Start(ctx context.Context) error {
 	go func() {
 		<-ctx.Done()
 
-		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		shutdownCtx, cancel := context.WithTimeout(
+			context.Background(), 
+			5*time.Second
+		)
 		defer cancel()
 
 		server.Shutdown(shutdownCtx)
